c4cee152ffd2f795ca7b4487fcea8fdc2796de41
Eduardo de la Garza <delagarzaguerra@gmail.com>
2017-09-16 17:56:49 -0500

    changed database

--- src/common/database.py
+++ src/common/database.py
@@ -11,7 +11,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client.get_default_database()
+        Database.DATABASE = "heroku_q93fqr67"
 
     @staticmethod
     def insert(collection, data):
